a14bf91050f6a81ea3c00364f46232e27813f43a
stecy1 <felisterpaul4@gmail.com>
2023-06-08 23:38:13 +0300

    Assignments on API Testing and Test Automation

---
+++ Assignment/learning.js
@@ -0,0 +1,32 @@
+http.get(`${url}/cart`) -concatination.
+
+const url = "http://test.com"
+
+function home(){
+    console.log(url);
+}
+
+
+function cart(){
+    console.log(`${url}/cart`);
+}
+
+cart()
+
+https://github.com/kamauvick/K6Auto
+// Send a POST request using K6 to this url
+// http://test.k6.io/login
+// Use this as the login and password
+// email: 'aaa',
+//     password: 'bbb',
+
+// headers
+// converting data to json and strings
+
+// https://github.com/airbnb/javascript
+// protobuf
+// protocol buffers
+
+// kafka
+// confluentS
+// message queues
