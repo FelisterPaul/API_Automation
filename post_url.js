595d91c9da0444250094db498d338b2ecda812a9
stecy1 <felisterpaul4@gmail.com>
2023-06-13 21:53:59 +0300

    add password and email

--- post_url.js
+++ post_url.js
@@ -3,8 +3,8 @@ import http from 'k6/http';
 export default function () {
   const url = 'https://renditionproperties.com/my-account';
   const payload = JSON.stringify({
-    email: 'aaa',
-    password: 'bbb',
+    email: 'felisterpaul4@gmail.com',
+    password: 'Aminahnjie17@',
   });
 
   const params = {
